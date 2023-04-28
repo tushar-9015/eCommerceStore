0f2f4ca5a4d58db95bb0b556324ad77b94a84964
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-04-28 23:59:03 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { persistor, store } from './Redux/store.jsx';
+
+import { PersistGate } from "redux-persist/integration/react";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-       <App />
-    </BrowserRouter>
+   <Provider store={store}>
+      <PersistGate loading={"loading"} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import './Cart.css';
+import React from 'react'
+import './cart.css'
+import { MdDeleteOutline } from 'react-icons/md'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeItem, resetCart } from '../../redux/cartReducer'
+import { makeRequest } from "../../makeRequest";
+
 
 const Cart = () => {
+  const dispatch = useDispatch()
+  const products = useSelector((state)=>state.cart.products)
+
+  const totalPrice = () => {
+    let total = 0;
+    products.forEach((item)=> (total += item.quantity*item.price));
+    return total.toFixed(2);
+  }
+
   return (
-    <div>
-      cart
+    <div className='cart'>
+      <h1>Products in your cart</h1>
+      {products?.map((item) => (
+        <div className='cart-item' key={item.id}>
+          <img src={import.meta.env.VITE_REACT_APP_API_UPLOAD_URL + item.img} alt='' />
+          <div className='cart-item-details'>
+             <h1>{item.title}</h1>
+             <p>{item.desc?.substring(0, 100)}</p>
+             <div className='cart-item-price'>{item.quantity} x ${item.price}</div>
+          </div>
+          <MdDeleteOutline className='cart-item-delete' onClick={()=>dispatch(removeItem(item.id))}/>
+        </div>
+      ))}
+      <div className='cart-item-total'>
+        <span>SUBTOTAL</span>
+        <span>{totalPrice()}</span>
+      </div>
+      <button onClick={handlePayment} className='cart-cheackout-btn'>PROCEED TO CHECKOUT</button>
+      <span className='cart-reset'onClick={()=>dispatch(resetCart())}>
+        RESET CART
+      </span>
     </div>
   )
 }
